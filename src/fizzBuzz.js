de18331b6a658e0377b261473600f3864f22e4bd
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2017-01-30 18:51:17 +0000

    first commit

---
+++ src/fizzBuzz.js
@@ -0,0 +1,34 @@
+var Fizzbuzz = function() {};
+
+Fizzbuzz.prototype.isDivisibleByThree = function(number) {
+  return this._isDivisibleBy(number, 3);
+};
+
+Fizzbuzz.prototype.isDivisibleByFive = function(number) {
+  return this._isDivisibleBy(number, 5);
+};
+
+Fizzbuzz.prototype.isDivisibleByFifteen = function(number) {
+  return this._isDivisibleBy(number, 15);
+};
+
+Fizzbuzz.prototype._isDivisibleBy = function(number, divisor) {
+  return (number % divisor === 0);
+};
+
+Fizzbuzz.prototype.says = function(number) {
+
+  if (this.isDivisibleByFifteen(number)) {
+    return "FizzBuzz"
+  };
+
+  if (this.isDivisibleByThree(number)) {
+    return "Fizz";
+  };
+
+  if (this.isDivisibleByFive(number)) {
+    return "Buzz";
+  };
+
+  return number;
+};
